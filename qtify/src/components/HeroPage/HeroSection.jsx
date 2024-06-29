89f2e29b3cd1dc4deac74a34a9267d1be64d5f85
MeghaNaut <meghanautiyal20@gmail.com>
2024-06-29 15:45:55 +0530

    hero correction

--- qtify/src/components/HeroPage/HeroSection.jsx
+++ qtify/src/components/HeroPage/HeroSection.jsx
@@ -4,19 +4,17 @@ import Headphone from "../../assets/headphone.svg";
 
 const HeroSection = () => {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.heroImageWrapper}>
-          <div className={styles.banner}>
-            <h1>100 Thousand Songs, ad fee</h1>
-            <h1>Over thhousands podcast episodes</h1>
-          </div>
-          <div>
-            <img src={Headphone} alt="Headphone" height="212px" />
-          </div>
+    <div className={styles.wrapper}>
+      <div className={styles.heroImageWrapper}>
+        <div className={styles.banner}>
+          <h1>100 Thousand Songs, ad-free</h1>
+          <h1>Over thousands of podcast episodes</h1>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={Headphone} alt="Headphone" className={styles.headphoneImage} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
