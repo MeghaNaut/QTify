2d6353f31e123fe96b78cf353d2a251d0fb8734c
MeghaNaut <meghanautiyal20@gmail.com>
2024-06-29 13:55:03 +0530

    nav correction2

--- qtify/src/components/NavBar/NavBar.jsx
+++ qtify/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./NavBar.module.css";
 import Logo from "../Logo/Logo";
-import SearchBar from "../SearchBar/SearchBar.jsx";
+import SearchBar from "../SearchBar/SearchBar";
 import FeedBackModal from "../FeedBackModal/FeedBackModal";
 import { useNavigate } from "react-router-dom";
 
@@ -9,33 +9,30 @@ const NavBar = ({ data, logo = false, search = false, feedback = false }) => {
   const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
   const navigate = useNavigate();
 
-  const toggleFeedBackModal = (value = false) => {
-    setIsFeedBackModalOpen(value);
+  const toggleFeedBackModal = () => {
+    setIsFeedBackModalOpen(!isFeedBackModalOpen);
   };
 
   return (
-    <div className={styles.navbarWrapper}> {/* Common parent wrapper */}
+    <div className={styles.navbarWrapper}>
       <div className={styles.wrapper}>
         <nav className={styles.navbar}>
-          <div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
-            {logo ? <Logo id={styles.logo} /> : null}
+          <div className={styles.logoWrapper} onClick={() => navigate("/")}>
+            {logo && <Logo id={styles.logo} />}
           </div>
-          {search ? (
+          {search && (
             <div className={styles.searchWrapper}>
               <SearchBar placeholder="Search an album of your choice" data={data} />
             </div>
-          ) : null}
-          {feedback ? (
-            <div className={styles.nav_link} onClick={() => toggleFeedBackModal(true)}>
+          )}
+          {feedback && (
+            <div className={styles.nav_link} onClick={toggleFeedBackModal}>
               Feedback
             </div>
-          ) : null}
+          )}
         </nav>
       </div>
-      <FeedBackModal
-        isOpen={isFeedBackModalOpen}
-        onDismiss={() => toggleFeedBackModal(false)}
-      />
+      <FeedBackModal isOpen={isFeedBackModalOpen} onDismiss={toggleFeedBackModal} />
     </div>
   );
 };
