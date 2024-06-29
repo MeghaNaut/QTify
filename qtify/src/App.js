33ef58ac1eeb56776a84807dc3a63cf6a60cd6e3
MeghaNaut <meghanautiyal20@gmail.com>
2024-06-29 12:47:26 +0530

    albums and feedback added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,16 +1,17 @@
+import React from "react"
 
-import './App.css';
-import Nav from './component/Nav';
-import Hero from './component/Hero';
-function App() {
+import { Route,Routes } from "react-router-dom"
+import LandingPage from "./mainPages/LandingPage";
+import AlbumsDetails from "./mainPages/AlbumsDetails/AlbumsDetails";
+
+const App = () => {
   return (
-    <div className="App">
-     <>
-     <Nav/>
-     <Hero/>
-     </>
-    </div>
-  );
+    <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/album/:slug" element={<AlbumsDetails />} />
+    <Route path="*" element={<LandingPage />} />
+  </Routes>
+  )
 }
 
-export default App;
+export default App
