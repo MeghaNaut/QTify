67ce843328204c102253569988fc7e6cf2bbd315
MeghaNaut <meghanautiyal20@gmail.com>
2024-06-29 15:41:00 +0530

    nav correcting

--- qtify/src/components/NavBar/NavBar.jsx
+++ qtify/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./NavBar.module.css";
 import Logo from "../Logo/Logo";
-import SearchBar from "../SearchBar/SearchBar";
+import SearchBar from "../SearchBar/SearchBar.jsx";
 import FeedBackModal from "../FeedBackModal/FeedBackModal";
 import { useNavigate } from "react-router-dom";
 
@@ -9,30 +9,33 @@ const NavBar = ({ data, logo = false, search = false, feedback = false }) => {
   const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
   const navigate = useNavigate();
 
-  const toggleFeedBackModal = () => {
-    setIsFeedBackModalOpen(!isFeedBackModalOpen);
+  const toggleFeedBackModal = (value = false) => {
+    setIsFeedBackModalOpen(value);
   };
 
   return (
-    <div className={styles.navbarWrapper}>
-      <div className={styles.wrapper}>
-        <nav className={styles.navbar}>
-          <div className={styles.logoWrapper} onClick={() => navigate("/")}>
-            {logo && <Logo id={styles.logo} />}
+    <div className={styles.wrapper}>
+      <nav className={styles.navbar}>
+        {logo && (
+          <div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
+            <Logo id={styles.logo} />
           </div>
-          {search && (
-            <div className={styles.searchWrapper}>
-              <SearchBar placeholder="Search an album of your choice" data={data} />
-            </div>
-          )}
-          {feedback && (
-            <div className={styles.nav_link} onClick={toggleFeedBackModal}>
-              Feedback
-            </div>
-          )}
-        </nav>
-      </div>
-      <FeedBackModal isOpen={isFeedBackModalOpen} onDismiss={toggleFeedBackModal} />
+        )}
+        {search && (
+          <div className={styles.searchWrapper}>
+            <SearchBar placeholder="Search an album of your choice" data={data} />
+          </div>
+        )}
+        {feedback && (
+          <button className={styles.nav_link} onClick={() => toggleFeedBackModal(true)}>
+            Give Feedback
+          </button>
+        )}
+      </nav>
+      <FeedBackModal
+        isOpen={isFeedBackModalOpen}
+        onDismiss={toggleFeedBackModal}
+      />
     </div>
   );
 };
